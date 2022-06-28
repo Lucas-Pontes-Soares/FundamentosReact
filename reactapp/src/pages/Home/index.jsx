a88f175b42eea7e7866fbc005e62954e0c8ec706
Lucas <lucas.soares177@etec.sp.gov.br>
2022-06-28 11:35:20 -0300

    Terminando as aulas do modulo

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,20 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import {Card} from '../../components/Card';
 
 export function Home() {
   const [studentName, setStudentName] = useState("Nome");
   {/* const [ondeConteudoFicaArmazenado, funcaoQueAtualizaEstado*/}
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: "", avatar: ""});
+
+  function handleAddStudent(){
+
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    };
+   
+    setStudents(prevState => [...prevState, newStudent]);
+    //pegar o estado anterior e tirar ele do vetor (...) + o novo estudante
+  }
+
+  //useEffect é executado 1 vez quando a pagina carrega
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch("https://api.github.com/users/Lucas-Pontes-Soares")
+      const data = await response.json()
+      console.log(data);
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    }
+    fetchData();
+  }, [students]); 
+  //vai ser executado também quando o estado students mudar
 
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
+      <header>
+          <h1>Lista de Presença</h1>
+          <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Imagem do professor" />
+      </div>
+      </header>
       <input type="text" placeholder="Digite o nome" onChange={e => setStudentName(e.target.value)} />
       {/* Pegando o valor do input e a cada alteração vai exibir*/}
-      <button type="button">Enviar</button>
-      <Card name={studentName} time="10:11:22"/>
-      <Card name="Lucas" time="15:10:09"/>
-      <Card name="Rodrigo" time="12:05:02"/>
+      <button type="button" onClick={handleAddStudent}>Enviar</button>
+     {
+      students.map(student => (
+      <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+      />
+      ))
+     }
+     
     </div>
   )
 }
